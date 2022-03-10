06524e7d85d7598af0a47d0de3770b704bed0a41
anchit1501 <1501.anchit@gmail.com>
2022-03-10 11:50:40 +1100

    Open weather API integrated

--- index.js
+++ index.js
@@ -2,13 +2,12 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 const app = express();
 
-app.get('/api',(req,res)=>{
-    res.json({success:true})
-})
+// Routes
+app.use("/api", require("./routes"));
 
 app.use(cors());
 
